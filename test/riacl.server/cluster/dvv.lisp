810da3d1db279be8f0ab82d267d8368be05c7a7b
certainty <david.krentzlin@gmail.com>
2024-04-07 20:11:27 +0200

    Add more unit tests for merge*

--- test/riacl.server/cluster/dvv.lisp
+++ test/riacl.server/cluster/dvv.lisp
@@ -12,7 +12,6 @@
         (b (dvv:make-dotted-version-vector)))
     (assert-true t)))
 
-
 (define-test merge-works ()
   (let* ((c1 (dvv:make-dotted-version-vector
               :initial-history (list
@@ -38,3 +37,41 @@
            ;; event 3 3 is removed for vnode-4
            (:actor-id ,+vnode-4+ :counter 4 :timestamp 4))
          history)))))
+
+(define-test merge-less-left ()
+  (let* ((c1 (dvv:make-dotted-version-vector :initial-dot (dvv:dot +vnode-1+ :counter 1 :timestamp 1)))
+         (c2 (dvv:make-dotted-version-vector :initial-history
+                                             (list (dvv:dot +vnode-2+ :counter 2 :timestamp 2))
+                                             :initial-dot (dvv:dot +vnode-3+ :counter 3 :timestamp 3)))
+         (c1+c2 (dvv:merge* c1 c2)))
+    (assert-true (null (dvv:dotted-version-vector-dot c1+c2)))
+    (assert-equal
+     `((:actor-id ,+vnode-1+ :counter 1 :timestamp 1)
+       (:actor-id ,+vnode-2+ :counter 2 :timestamp 2)
+       (:actor-id ,+vnode-3+ :counter 3 :timestamp 3))
+     (mapcar #'prelude:to-plist (dvv:dotted-version-vector-history-sorted c1+c2)))))
+
+(define-test merge-less-right ()
+  (let* ((c1 (dvv:make-dotted-version-vector :initial-history (list (dvv:dot +vnode-2+ :counter 2 :timestamp 2))
+                                             :initial-dot (dvv:dot +vnode-3+ :counter 3 :timestamp 3)))
+         (c2 (dvv:make-dotted-version-vector :initial-dot (dvv:dot +vnode-1+ :counter 1 :timestamp 1)))
+         (c1+c2 (dvv:merge* c1 c2)))
+    (assert-true (null (dvv:dotted-version-vector-dot c1+c2)))
+    (assert-equal
+     `((:actor-id ,+vnode-1+ :counter 1 :timestamp 1)
+       (:actor-id ,+vnode-2+ :counter 2 :timestamp 2)
+       (:actor-id ,+vnode-3+ :counter 3 :timestamp 3))
+     (mapcar #'prelude:to-plist (dvv:dotted-version-vector-history-sorted c1+c2)))))
+
+(define-test merge-same-id ()
+  (let* ((c1 (dvv:make-dotted-version-vector :initial-history (list (dvv:dot +vnode-1+ :counter 1 :timestamp 2))
+                                             :initial-dot (dvv:dot +vnode-2+ :counter 1 :timestamp 4)))
+         (c2 (dvv:make-dotted-version-vector :initial-history (list (dvv:dot +vnode-1+ :counter 1 :timestamp 3))
+                                             :initial-dot (dvv:dot +vnode-3+ :counter 1 :timestamp 5)))
+         (c1+c2 (dvv:merge* c1 c2)))
+    (assert-true (null (dvv:dotted-version-vector-dot c1+c2)))
+    (assert-equal
+     `((:actor-id ,+vnode-1+ :counter 1 :timestamp 3)
+       (:actor-id ,+vnode-2+ :counter 1 :timestamp 4)
+       (:actor-id ,+vnode-3+ :counter 1 :timestamp 5))
+     (mapcar #'prelude:to-plist (dvv:dotted-version-vector-history-sorted c1+c2)))))
